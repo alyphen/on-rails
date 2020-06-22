4d823e28c25b5c0dd6f42cd64d1315b69802724a
Ren Binden <ren.binden@gmail.com>
2020-06-22 07:32:06 +0100

    Add avatars

--- src/main/kotlin/uk/co/renbinden/onrails/avatar/Avatars.kt
+++ src/main/kotlin/uk/co/renbinden/onrails/avatar/Avatars.kt
@@ -5,6 +5,9 @@ import uk.co.renbinden.onrails.assets.Assets
 class Avatars(val assets: Assets) {
 
     val angelaFunikular = Avatar("Angela Funikular", assets.images.portraitAngelaFunikular)
+    val dianaBogie = Avatar("Diana Bogie", assets.images.portraitDianaBogie)
     val jasonHardrail = Avatar("Jason Hardrail", assets.images.portraitJasonHardrail)
+    val luluMomentis = Avatar("Lulu Momentis", assets.images.portraitLuluMomentis)
+    val mauriceSourth = Avatar("Maurice Sourth", assets.images.portraitMauriceSourth)
 
 }
